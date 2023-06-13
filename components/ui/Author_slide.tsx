1e0a13cfe613411752ce941db74c60d87da54b87
Mikha38 <oyks1379@yandex.ru>
2023-06-13 16:58:02 +0300

    Верстка

--- components/ui/Author_slide.tsx
+++ components/ui/Author_slide.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Author_slide( { name, src }: { name: string, src: string} ){
     return(
-        <iframe src={src} title={name} loading='lazy' className="bg-[#D9D9D9] w-full aspect-video"  allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
+        <iframe src={src} title={name} loading='lazy' className="bg-[#D9D9D9] w-full aspect-video"  />
     )
 }
